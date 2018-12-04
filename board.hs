4b562ce7df552ad3be30edfe004272182980638a
marknunez1997@gmail.com <Your Email Address Here>
2018-12-04 14:18:36 -0700

    make players and change turns added

--- board.hs
+++ board.hs
@@ -4,12 +4,24 @@
 -- CS3360 TR 10:30 - 11:50 
 -- 12/06/18
 
-module Board(mkBoard) where
+module Board(mkBoard, mkPlayer, mkOpponent, changeTurn) where
 
 import Data.List
 import Data.Array
 
 -- Generates a default mxn board containing 0's
 mkBoard :: Int -> Int -> [[Int]]
-mk Board m n = replicate m (replicate n 0) 
+mkBoard m n = replicate m (replicate n 0) 
 
+-- making players
+mkPlayer :: Int
+mkPlayer = 1
+
+mkOpponent :: Int
+mkOpponent = 2
+
+--taking turns
+changeTurn :: Int -> Int
+changeTurn turn
+     |turn == 1 = mkOpponent
+     |otherwise = mkPlayer
--- main.hs
+++ main.hs
@@ -1,1 +1,6 @@
-s
+-- Mauricio Roberto Hidalgo & Mark Hiram Nunez
+-- Programming Assignment 3
+-- Haskell
+-- CS3360 TR 10:30 - 11:50
+-- 12/06/18
+
