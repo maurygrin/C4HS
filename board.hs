7826a8f6f55b982bd777a64421c6260f4cd7785d
maurygrin <43419647+maurygrin@users.noreply.github.com>
2018-12-04 13:54:44 -0700

    mkBoard

--- board.hs
+++ board.hs
@@ -1,1 +1,15 @@
-s
+-- Mauricio Roberto Hidalgo & Mark Nunez 
+-- Programming Assignment 3
+-- Haskell
+-- CS3360 TR 10:30 - 11:50 
+-- 12/06/18
+
+module Board(mkBoard) where
+
+import Data.List
+import Data.Array
+
+-- Generates a default mxn board containing 0's
+mkBoard :: Int -> Int -> [[Int]]
+mk Board m n = replicate m (replicate n 0) 
+
