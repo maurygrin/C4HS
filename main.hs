a888eb1b294d80af9059164d294214327efba303
maurygrin <43419647+maurygrin@users.noreply.github.com>
2018-12-04 22:02:31 -0700

    playerToChar added

--- main.hs
+++ main.hs
@@ -4,3 +4,18 @@
 -- CS3360 TR 10:30 - 11:50
 -- 12/06/18
 
+import Data.List
+import System.IO
+import Board
+
+-- Main of the proyect that starts the game
+main = do
+     let bd = mkBoard 7 6
+     let p = mkPlayer
+ 
+-- Return a char representation of a player p.
+playerToChar :: Int -> String
+playerToChar p 
+     | (p == 1) = "O "
+     | (p == 2) = "X "
+     | otherwise = ". "
